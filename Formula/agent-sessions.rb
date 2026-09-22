90e94bc7e51a6b3be1c42da71de567289c5bbd80
Jared McGuire <jrdmcgr@gmail.com>
2026-09-22 10:46:04 -0400

    Fix output binary name

--- Formula/agent-sessions.rb
+++ Formula/agent-sessions.rb
@@ -8,8 +8,8 @@ class AgentSessions < Formula
   depends_on "go" => :build
 
   def install
-    ldflags = "-s -w -X main.version=#{version}"
-    system "go", "build", *std_go_args(ldflags: ldflags), "."
+    ldflags = "-X main.version=#{version}"
+    system "go", "build", *std_go_args(output: bin/"sessions", ldflags: ldflags), "."
   end
 
   test do
